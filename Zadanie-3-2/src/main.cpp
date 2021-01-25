eabee9e2da2b4820851a2ecc76237179b8545662
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2021-01-25 09:22:08 +0100

    Wykonano zadanie 3.2.2 –sterowanie ekranami

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,33 +1,51 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
+#define buttonUp 11
+#define buttonDown 13
+
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 int menu=1;
 
 void setup(void) {
   lcd.begin(16, 2);
+  pinMode(buttonUp, INPUT_PULLUP);
+  pinMode(buttonDown, INPUT_PULLUP);
 }
 
 void loop(void) {
-  dispMenu();
-  delay(1000);
-  menu++;
-  if(menu>3) menu=1;
+ dispMenu();
+ changeMenu();
 }
 
 void dispMenu(void){
   switch(menu){
   case 1:
-  lcd.setCursor(0,0);
-  lcd.print("Menu 1");
-  break;
+    lcd.setCursor(0,0);
+    lcd.print("Menu 1");
+    break;
   case 2:
-  lcd.setCursor(0,0);
-  lcd.print("Menu 2");
-  break;
+    lcd.setCursor(0,0);
+    lcd.print("Menu 2");
+    break;
   case 3:
   lcd.setCursor(0,0);
   lcd.print("Menu 3");
   break;
+  }
+}
+
+void changeMenu(void){
+  if(digitalRead(buttonUp) == HIGH) {
+    menu=menu+1;
+    if(menu>1){
+      menu=1;
+    }
+  }
+  if(digitalRead(buttonDown) == HIGH){
+    menu=menu-1;
+    if(menu=1){
+      menu=3;
+    }
   }
 }

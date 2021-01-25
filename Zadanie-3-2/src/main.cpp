f07ed56c832f4ed13bf1b56c99e4cd9b8cacd398
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2021-01-25 10:07:38 +0100

    Wykonano zadanie 3.2.4 - odczyt temperatury

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,58 +1,71 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-#define buttonUp 11 // przycisk +
-#define buttonDown 13 // przycisk -
-
+#define buttonUp    11 // przycisk +
+#define buttonDown  13 // przycisk -
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 int menu=1;
-bool psButtonUp;
-bool psButtonDown
-  ;
-void setup(void) {
+bool psButtonUp = LOW;
+bool psButtonDown = LOW;
+float temperature;
+
+void lcdClear(void);
+
+void changeMenu(void){
+  if(digitalRead(buttonUp) == HIGH && psButtonUp == LOW){
+    psButtonUp = HIGH;
+    menu=menu+1;
+    if(menu>3){
+      menu=1;
+    }
+    lcd.clear();
+  }
+  if(digitalRead(buttonDown) == HIGH && psButtonDown == LOW) {
+    psButtonDown = HIGH;
+    menu=menu-1;
+    if(menu<1){
+      menu=3;
+    }
+    lcd.clear();
+  }
+  psButtonUp = digitalRead(buttonUp);
+  psButtonDown = digitalRead(buttonDown);
+}
+
+void setup(void){
   lcd.begin(16, 2);
   pinMode(buttonUp, INPUT_PULLUP);
   pinMode(buttonDown, INPUT_PULLUP);
 }
 
 void loop(void) {
- dispMenu();
- changeMenu();
+  dispMenu();
+  changeMenu();
+  readTemperature();
+}
+
+void readTemperature(void){
+  unsigned int digital = analogRead(A5);
+  float resolution = (5.0f / 1024.0f);
+  float voltage = resolution * digital;
+  temperature = (voltage-0.1f) * (125.0f+40.0f) / (1.75f-0.1f) - 40.0f;
 }
 
 void dispMenu(void){
   switch(menu){
   case 1:
     lcd.setCursor(0,0);
-    lcd.print("Menu 1");
+    lcd.print("Temperatura:");
+    lcd.setCursor(0,1);
+    lcd.print(temperature);
     break;
   case 2:
     lcd.setCursor(0,0);
     lcd.print("Menu 2");
     break;
   case 3:
-  lcd.setCursor(0,0);
-  lcd.print("Menu 3");
-  break;
+    lcd.setCursor(0,0);
+    lcd.print("Menu 3");
+    break;
   }
-}
-
-void changeMenu(void){
-  if(digitalRead(buttonUp) == HIGH && psButtonDown == LOW) {
-    psButtonDown = HIGH;
-    menu=menu+1;
-    if(menu>3){
-      menu=1;
-    }
-  }
-  if(digitalRead(buttonDown) == HIGH && psButtonDown == LOW){
-    psButtonDown = HIGH;
-    menu=menu-1;
-    if(menu<1){
-      menu=3;
-    }
-  }
-  psButtonUp = digitalRead(buttonUp);
-  psButtonDown = digitalRead(buttonDown);
-  
 }
